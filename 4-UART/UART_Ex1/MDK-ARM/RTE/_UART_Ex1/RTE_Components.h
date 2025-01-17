0e06854b0425e8a88e7785d0bae9c84e788a4b19
TohidMoosazadeh <moosazadeh.tohid@gmail.com>
2025-01-17 14:40:18 +0330

    UART Projects

---
+++ 4-UART/UART_Ex1/MDK-ARM/RTE/_UART_Ex1/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'UART_Ex1' 
+ * Target:  'UART_Ex1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
